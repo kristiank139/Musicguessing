618b091d83609e897c14a43b27ada845f44a1435
Kristian <105385905+kristiank139@users.noreply.github.com>
2022-12-30 17:20:31 +0200

    Updated main.py
    Created file renaming, fixed bugs, improved composition, need to clean up code and find more possible bugs

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import threading
 
 root = Tk()
 
-global files, i, guessed, link, guess
+global files, i, guessed, link, guess, authorSet, genreSet, titleSet
 
 files = []
 i = -1
@@ -42,7 +42,9 @@ def Download():
     else:
         video = pafy.new(link.get())
         audio = video.audiostreams
-        audio[3].download(filepath="/Users/krist/Documents/Programmeerimine/Serious projects/Musicguessing")
+        audio[3].download(filepath=f"{os.getcwd()}/songs")
+    
+
     
 # Plays a random song from the folder "songs"
 def Play():
@@ -51,7 +53,9 @@ def Play():
         global randinteger, filename
         randinteger = random.randint(0, i)
         filename = files[randinteger].split(".")
-        p = vlc.MediaPlayer("/Users/krist/Documents/Programmeerimine/Serious projects/Musicguessing/songs/" + files[randinteger])
+        filename.pop(0)
+        filename.pop(-1)
+        p = vlc.MediaPlayer(f"{os.getcwd()}/songs/{files[randinteger]}")
         p.audio_set_volume(62)
         p.play()
 
@@ -62,11 +66,11 @@ def Play():
             pass
 
         files.remove(files[randinteger])
-        print(f"All {i} songs played")
         i -= 1
         p.stop()
         time.sleep(1)
         setFalse(guessed)
+    print(f"All {i} songs played")
 
 # Checks if the guess is correct
 
@@ -76,8 +80,6 @@ def setFalse(guess):
 def guessChecker():
 
     # Segregating the author, song name and genre
-    filename.pop(0)
-    filename.pop(-1)
     for n in filename:
         if len(n.split("-")) > 1:
             print(n.split("-"))
@@ -91,7 +93,6 @@ def guessChecker():
 
     global guessed
     setFalse(guessed)
-    print(guessed)
 
     if guess.get().lower() == author.lower() + " " + type.lower() + " " + name.lower():
             guessed = True
@@ -99,8 +100,14 @@ def guessChecker():
     time.sleep(0.9)
     guessed = False
 
+def fileRename():
+    old_name = f"{os.getcwd()}/songs/{files[randinteger]}"
+    replaced = f"{authorSet.get()}.{genreSet.get()}.{titleSet.get()}".replace(" ", "-")
+    new_name = f"{os.getcwd()}/songs/.{replaced}.webm"
+    os.rename(old_name, new_name)
+    
 # Tkinter
-root.geometry("800x500")
+root.geometry("800x550")
 root.resizable(1,1)
 root.eval('tk::PlaceWindow . center')
 root.title("Song guesser")
@@ -108,21 +115,32 @@ root.title("Song guesser")
 # Inputs
 link = StringVar()
 guess = StringVar()
+authorSet = StringVar()
+genreSet = StringVar()
+titleSet = StringVar()
+
 
 # Labels
 Label(root, text="Enter song link or playlist: ").place(x=160, y=30)
+Label(root, text="Author: ").place(x=50, y=330)
+Label(root, text="Genre: ").place(x=50, y=380)
+Label(root, text="Title: ").place(x=50, y=430)
+Label(root, text="Identify songs: ").place(x=50, y=290)
 
 # Buttons
 Button(root, text="Download", command=Download).place(x=595, y=58)
+Button(root, text="Rename file", command=fileRename).place(x=50, y=480)
 play = Button(root, text="Play song", command=threading.Thread(target=Play).start).place(x=60, y=135)
-
 
 # Entries
 linkEnter = Entry(root, width = 60, textvariable = link).place(x=25, y=60)
+authorEnter = Entry(root, width = 15, textvariable = authorSet).place(x=150, y=330)
+genreEnter = Entry(root, width = 15, textvariable = genreSet).place(x=150, y=380)
+titleEnter = Entry(root, width = 15, textvariable = titleSet).place(x=150, y=430)
 
 # Guessing GUI
-Label(root, text="Guess the song author, type/genre and name(ex. creator Spiritus Gregoriuse koraal Veni): ").place(x=32, y=220)   
-Button(root, text="Guess", command = guessChecker).place(x = 595, y = 258)
-Entry(root, width = 60, textvariable = guess).place(x=25, y=260)
+Label(root, text="Guess the song author, type/genre and name(ex. creator Spiritus Gregoriuse koraal Veni): ").place(x=32, y=200)   
+Button(root, text="Guess", command = guessChecker).place(x = 595, y = 239)
+Entry(root, width = 60, textvariable = guess).place(x=25, y=240)
 
 root.mainloop()
