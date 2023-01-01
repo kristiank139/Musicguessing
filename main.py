e0cda789a9e49f3b0bcc827a1c1aae90f747999e
Kristian <105385905+kristiank139@users.noreply.github.com>
2023-01-01 08:15:04 +0200

    Improvement update
    Seperators, fixed bugs, better way to name files and guess them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 from pytube import YouTube, Playlist
 import vlc
 import time
@@ -9,7 +10,7 @@ import threading
 
 root = Tk()
 
-global files, i, guessed, link, guess, authorSet, genreSet, titleSet
+global files, i, guessed, link, guess, titleSet
 
 files = []
 i = -1
@@ -32,7 +33,7 @@ def MediaParsedChanged(media):
 def Download():
     if not os.path.exists(f"{os.getcwd()}/songs"):
         os.mkdir("songs")
-    if "playlist" in link.get():
+    if "playlist" in link.get() and link.get():
         p = Playlist(str(link.get()))
         if len(p.video_urls) == 0:
             print("Empty or private playlist, change playlist to public.")
@@ -40,12 +41,14 @@ def Download():
             video = pafy.new(url)
             audio = video.audiostreams
             audio[3].download(filepath=f"{os.getcwd()}/songs")
-    else:
+    elif "youtube.com" in link.get():
         video = pafy.new(link.get())
         audio = video.audiostreams
         audio[3].download(filepath=f"{os.getcwd()}/songs")
-    
-
+    elif not link.get():
+        print("Enter a link!")
+    else:
+        print("Not a Youtube link")
     
 # Plays a random song from the folder "songs" until there are no more songs to play
 def Play():
@@ -65,7 +68,6 @@ def Play():
         # Waiting for it to start playing
         while MediaParsedChanged(p) == 0:
             time.sleep(0.1)
-
         # Infinite loop that will break once guessed is equal to True
         while guessed == False:
             pass
@@ -75,7 +77,7 @@ def Play():
         p.stop()
         time.sleep(1)
         setFalse(guessed)
-    print(f"All {i} songs played")
+    print(f"All songs played!")
 
 def setFalse(g):
     g = False
@@ -91,21 +93,27 @@ def guessChecker():
 
     global guessed
     setFalse(guessed)
-    print(filename)
-
-    if guess.get().lower() == filename[0].lower() + " " + filename[1].lower() + " " + filename[2].lower():
+    print(guess, filename)
+    if guess.get().lower() == filename[0].lower() and guess.get():
             guessed = True
             print("Correct")
+    elif guess.get().lower() == filename[0].lower():
+        print("Wrong guess")
+    elif filename == None:
+        print("Song isn't playing yet!")
+    else:
+        print("Enter a guess")
     time.sleep(0.9)
     guessed = False
 
 def fileRename():
-    # Renaming the file with the right format
+
+ # Renaming the file with the right format
     old_name = f"{os.getcwd()}/songs/{files[randinteger]}"
-    replaced = f"{authorSet.get()}.{genreSet.get()}.{titleSet.get()}".replace(" ", "-").replace(":", " ").replace(";", "")
+    replaced = f"{titleSet.get()}".replace(" ", "-").replace(":", "").replace(";", "")
     new_name = f"{os.getcwd()}/songs/.{replaced}.webm"
     os.rename(old_name, new_name)
-    print(f"Filename set to {replaced}")
+    print(f"Filename set to .{replaced}.webm")
 
 def skipSong():
     global guessed
@@ -114,7 +122,7 @@ def skipSong():
     guessed = False
     
 # Tkinter
-root.geometry("800x550")
+root.geometry("800x450")
 root.resizable(1,1)
 root.eval('tk::PlaceWindow . center')
 root.title("Song guesser")
@@ -122,30 +130,33 @@ root.title("Song guesser")
 # Inputs
 link = StringVar()
 guess = StringVar()
-authorSet = StringVar()
-genreSet = StringVar()
 titleSet = StringVar()
 
 # Labels
 Label(root, text="Enter song link or playlist: ").place(x=160, y=30)
-Label(root, text="Author: ").place(x=50, y=330)
-Label(root, text="Genre: ").place(x=50, y=380)
-Label(root, text="Title: ").place(x=50, y=430)
+Label(root, text="Title: ").place(x=50, y=330)
 Label(root, text="Identify songs: ").place(x=50, y=290)
 Label(root, text="Guess the song author, type/genre and name(ex. creator Spiritus Gregoriuse koraal Veni): ").place(x=32, y=200)   
 
 # Buttons
 Button(root, text="Download", command=Download).place(x=595, y=58)
-Button(root, text="Rename file", command=fileRename).place(x=50, y=480)
+Button(root, text="Rename file", command=fileRename).place(x=50, y=380)
 play = Button(root, text="Play song", command=threading.Thread(target=Play).start).place(x=60, y=135)
 Button(root, text="Skip song", command=skipSong).place(x=230, y=135)
 Button(root, text="Guess", command = guessChecker).place(x = 595, y = 239)
 
 # Entries
 linkEnter = Entry(root, width = 60, textvariable = link).place(x=25, y=60)
-authorEnter = Entry(root, width = 15, textvariable = authorSet).place(x=150, y=330)
-genreEnter = Entry(root, width = 15, textvariable = genreSet).place(x=150, y=380)
-titleEnter = Entry(root, width = 15, textvariable = titleSet).place(x=150, y=430)
+titleEnter = Entry(root, width = 25, textvariable = titleSet).place(x=105, y=330)
+Entry(root, width = 60, textvariable = guess).place(x=25, y=240)
+
+# Separators
+ttk.Separator(root, orient='horizontal').place(x=0, y=100, width=800)
+ttk.Separator(root, orient='horizontal').place(x=0, y=185, width=800)
+ttk.Separator(root, orient='horizontal').place(x=0, y=285, width=800)
+ttk.Separator(root, orient='horizontal').place(x=0, y=425, width=800)
+
+root.mainloop()
 Entry(root, width = 60, textvariable = guess).place(x=25, y=240)
 
 root.mainloop()
